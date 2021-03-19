61938732178e09a168c867a3d33d33d6e1432009
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-03-19 17:36:57 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -92,8 +92,8 @@ async def upload_to_tg(
             d_f_s = humanbytes(os.path.getsize(local_file_name))
             i_m_s_g = await message.reply_text(
                 "Telegram does not support uploading this file.\n"
-                f"Detected File Size: {d_f_s} 😡\n"
-                "\n🤖 trying to split the files 🌝🌝🌚"
+                f"Detected File Size: {d_f_s} \n"
+                "\n🤖 trying to split the files 🌝🌚"
             )
             splitted_dir = await split_large_files(local_file_name)
             totlaa_sleif = os.listdir(splitted_dir)
@@ -102,7 +102,7 @@ async def upload_to_tg(
             LOGGER.info(totlaa_sleif)
             ba_se_file_name = os.path.basename(local_file_name)
             await i_m_s_g.edit_text(
-                f"Detected File Size: {d_f_s} 😡\n"
+                f"Detected File Size: {d_f_s} \n"
                 f"<code>{ba_se_file_name}</code> splitted into {number_of_files} files.\n"
                 "trying to upload to Telegram, now ..."
             )
@@ -263,7 +263,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
             # reply_to_message_id=message.reply_to_message.message_id,
             progress=progress_for_pyrogram,
             progress_args=(
-                "trying to upload",
+                "   ╭──────── ⌊ 📤 Uploading ⌉ ",
                 message_for_progress_display,
                 start_time
             )
@@ -356,7 +356,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
                         # reply_to_message_id=message.reply_to_message.message_id,
                         progress=progress_for_pyrogram,
                         progress_args=(
-                            "trying to upload",
+                            "   ╭──────── ⌊ 📤 Uploading ⌉ ",
                             message_for_progress_display,
                             start_time
                         )
@@ -412,7 +412,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
                         # reply_to_message_id=message.reply_to_message.message_id,
                         progress=progress_for_pyrogram,
                         progress_args=(
-                            "trying to upload",
+                            "   ╭──────── ⌊ 📤 Uploading ⌉ ",
                             message_for_progress_display,
                             start_time
                         )
@@ -454,7 +454,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user, e
                         # reply_to_message_id=message.reply_to_message.message_id,
                         progress=progress_for_pyrogram,
                         progress_args=(
-                            "trying to upload",
+                            "   ╭──────── ⌊ 📤 Uploading ⌉ ",
                             message_for_progress_display,
                             start_time
                         )

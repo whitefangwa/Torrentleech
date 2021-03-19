2452778609b411721b30e4b9eaad2de781cd2595
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-03-19 21:04:13 +0530

    Delete config.py

--- tobrot/config.py
+++
@@ -1,9 +0,0 @@
-from tobrot.sample_config import Config
-
-class Config(object):
-    TG_BOT_TOKEN = "1416418161:AAF-syqPkfUlX8RwkoXBSITZnZ4XgKfHmUQ"
-    APP_ID = 1616386
-    API_HASH = "2010b565a452bd869c044783d536aa60"
-    OWNER_ID = 750995704
-    AUTH_CHANNEL = [-1001354522142]
-    DOWNLOAD_LOCATION = "./DOWNLOADS"

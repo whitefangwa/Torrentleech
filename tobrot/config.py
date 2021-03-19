d0cf9653c6104a9e81efd0aebb6ecb1c09867c6a
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-03-19 21:04:48 +0530

    Update and rename g_config.py to config.py

---
+++ tobrot/config.py
@@ -0,0 +1,13 @@
+from tobrot.sample_config import Config
+
+#Fill your all data, read readme for reference
+
+#FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+
+class Config(Config):
+    TG_BOT_TOKEN = "1416418161:AAF-syqPkfUlX8RwkoXBSITZnZ4XgKfHmUQ"
+    APP_ID = 1616386
+    API_HASH = "2010b565a452bd869c044783d536aa60"
+    OWNER_ID = 750995704
+    AUTH_CHANNEL = [-1001354522142]
+    DOWNLOAD_LOCATION = "./DOWNLOADS"

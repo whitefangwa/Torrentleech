88c88b7d563edef14a5d8708a77559c3b2853542
mikiadarsh25 <57398892+mikiadarsh25@users.noreply.github.com>
2021-04-20 12:46:50 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -8,11 +8,10 @@ class Config(Config):
     AUTH_CHANNEL = [-1001354522142]
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     UPLOAD_AS_DOC = "True"
-    RCLONE_CONFIG = "[nubx]
-type = drive
+    RCLONE_CONFIG = """type = drive
 scope = drive
 token = {"access_token":"ya29.a0AfH6SMBoyiFJuaxTCH4OXOqL-J5UbFbSbDCe4twd8GQJL2n4MhVaFNokbI2RWEPXMgveL1H3-t3rvaZjBTS5l0UwawRmrSh6h-oAo5OrSEol4smigYYnqSCz1nmJUvJ5DPSf9gYuf-dDmxMobAAksfHZzlwR","token_type":"Bearer","refresh_token":"1//0gNFt-5wz3WG8CgYIARAAGBASNwF-L9IrRgD2fFsZor0XAeDCeDCaLO-64VFApO84leihFhHv7ZHOoHFyIcHoL8T39eqNSeIruAA","expiry":"2021-04-19T21:47:07.5921471+05:30"}
 team_drive = 0AMG7cLm-HKhMUk9PVA
-root_folder_id = 1qKOJ7rn-2ShwPe2XxYy2iLiB2VhZIJr3"
+root_folder_id = 1qKOJ7rn-2ShwPe2XxYy2iLiB2VhZIJr3"""
     DESTINATION_FOLDER = "leecher"
     INDEX_LINK = "https://mdrive.mikiadarsh25.workers.dev/0:/leecher"

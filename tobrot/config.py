b914c307538a242842fcc964b5daa915f57cb7b2
animematrix202 <81048921+animematrix202@users.noreply.github.com>
2021-04-20 14:03:50 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,17 +1,17 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-    TG_BOT_TOKEN = "1416418161:AAETSSR3BzErnpCwPNWpn4ZAVRpkw3IFoq0"
-    APP_ID = 1616386
-    API_HASH = "2010b565a452bd869c044783d536aa60"
+    TG_BOT_TOKEN = "1793399974:AAFvkuc_FdCsyhyFGWxtVZqXesfyAT2AUZc"
+    APP_ID = 2134383
+    API_HASH = "8092900454d217a045b4c11ebc992054"
     OWNER_ID = 750995704
-    AUTH_CHANNEL = [-1001354522142]
+    AUTH_CHANNEL = [-1001458018565]
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     UPLOAD_AS_DOC = "True"
     RCLONE_CONFIG = """type = drive
 scope = drive
-token = {"access_token":"ya29.a0AfH6SMBoyiFJuaxTCH4OXOqL-J5UbFbSbDCe4twd8GQJL2n4MhVaFNokbI2RWEPXMgveL1H3-t3rvaZjBTS5l0UwawRmrSh6h-oAo5OrSEol4smigYYnqSCz1nmJUvJ5DPSf9gYuf-dDmxMobAAksfHZzlwR","token_type":"Bearer","refresh_token":"1//0gNFt-5wz3WG8CgYIARAAGBASNwF-L9IrRgD2fFsZor0XAeDCeDCaLO-64VFApO84leihFhHv7ZHOoHFyIcHoL8T39eqNSeIruAA","expiry":"2021-04-19T21:47:07.5921471+05:30"}
-team_drive = 0AMG7cLm-HKhMUk9PVA
-root_folder_id = 1qKOJ7rn-2ShwPe2XxYy2iLiB2VhZIJr3"""
+token = {"access_token":"ya29.a0AfH6SMCYOHOUr0xmb9Cch0RPJsJvXTFf7H_UKqbx3jpI32aNvUwfdUXTcYDSkeOdSKKSUwQwf56OcpkHCXZu0IShkjf_NTk4aza2xHBKL2-RpblV-rVC95LK9c8bxH7xmEBFkNLCF6mquFF6ZFHIDZdXnkbs","token_type":"Bearer","refresh_token":"1//0gre87HsqQTvYCgYIARAAGBASNwF-L9IrVDfhTrvicncTd-oR6CuTWR9f4eLSzUJq1g4_ggccX7ktMUGeAdrwUQBBj5TWreRiyk0","expiry":"2021-04-20T14:55:13.5386895+05:30"}
+team_drive = 0AEOPWqM5rUlYUk9PVA
+root_folder_id = 1NuE3MWzMjrViizdoBZwAGy-kVExYYnWV"""
     DESTINATION_FOLDER = "leecher"
-    INDEX_LINK = "https://mdrive.mikiadarsh25.workers.dev/0:/leecher"
+    INDEX_LINK = "https://animematrix.mikiadarsh25.workers.dev/0:/leecher"
